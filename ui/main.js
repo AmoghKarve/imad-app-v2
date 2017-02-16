61d2c8ceef04a69f8fe50a6b1ddbd089e2ff0096
AmoghKarve <amogh.karve@gmail.com>
2017-02-16 21:16:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,8 +13,8 @@ var button = document.getElementById('counter');
 button.onclick = function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
-        if(request.readyState == XMLHttpRequest.DONE){
-            if(request.status == 200){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
                 var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
@@ -23,3 +23,6 @@ button.onclick = function(){
     };
 };
 var counter = 0;
+
+request.open('GET', ' http://amoghkarve.imad.hasura-app.io/counter');
+request.send(null);
