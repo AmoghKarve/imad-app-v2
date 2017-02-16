182e25f128f74a7c3e7033c5b498554616e4edf2
AmoghKarve <amogh.karve@gmail.com>
2017-02-16 22:09:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,13 @@ app.get('/counter',function(req,res){
    counter = counter + 1;
    res.send(counter.toString());
 });
+
+var names = [];
+app.get('/submit-name/:name',function(req,res){
+    var name = req.params.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
