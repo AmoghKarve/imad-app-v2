acf00ad378f2a1708f8fa31a73a1a01851623d22
AmoghKarve <amogh.karve@gmail.com>
2017-02-16 11:35:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-var img = document.getElementById('madi');
+/*var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveright(){
     marginLeft = marginLeft + 10;
@@ -8,4 +8,10 @@ function moveright(){
 }
 img.onclick = function () {
     var interval = setInterval(moveright,50);
-};
+};*/
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function () {
+    counter = counter + 1;
+    span.innerHTML = counter.toString();
+}
