945e212b82894bfe4e1cc18fea3eeb34bac9c1f7
AmoghKarve <amogh.karve@gmail.com>
2017-03-07 19:27:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+    user:"amoghkarve",
+    database:"amoghkarve",
+    host:"db.imad.hasura-app.io",
+    password:process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -92,6 +99,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } 
+       else{
+           res.send(JSON.stringify(result));
+       }
+    });
+});
 app.get('/:articleName', function(req,res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
